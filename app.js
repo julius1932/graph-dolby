024c2cd7326ecfc56a56d3c9cc41b9c98b8562a0
julius1932 <lasjulius@gmail.com>
2019-11-22 15:26:49 +0200

    update

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const app = express();
 const gsjson = require('google-spreadsheet-to-json');
 const bodyParser = require('body-parser');
 
+const TIERS = {
+    T1: ['LG', 'SONY', 'SAMSUNG'],
+    T2: ['TCL', 'VU', 'PANASONIC', 'SHARP', 'SKYWORTH', 'PHILIPS', 'HITACHI',
+        'SANSUI', 'JVC'
+    ],
+    T3: ['HAIER', 'LLOYD', 'RECONNECT', 'VU', 'LLOYD', 'ONIDA', 'KORYO', 'IMPEX',
+        'CROMA', 'AMSTRAD', 'MICROMAX', 'ADZEN', 'HENRY', 'IFFALCON', 'INTEX', 'METZ',
+        'MI', 'NVY', 'T-SERIES', 'VISE', 'PANORAMA', 'KODAK - 1', 'AKAI - 3', 'BELTEK - 2',
+        'DETEL - 4', 'MITASHI - 4', 'OSCAR - 1', 'DAIWA'
+    ]
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -30,17 +42,131 @@ app.get('/dist', function(req, res) {
     return HELPERS.distList(res);
 });
 //:retailerName?/:location?/:city?/:country?/:brandModel?/:brandName?/:category?/:trademark?/:threeFiles?/:pianoSound?/:distributor?/:countryOfOrigin?
-app.get(`/clients`, function(req, res) {
-   res.sendFile(__dirname + '/clients.html');
+app.get(`/client`, function(req, res) {
+    res.sendFile(__dirname + '/clients.html');
 });
 app.post(`/clients`, function(req, res) {
-   return HELPERS.clients(req, res);
+     return HELPERS.clientsExcel(req, res);
 });
+app.get(`/clients`, function(req, res) {
+    HELPERS.clients(req, res, function(result) {
+
+        let t1Data = result.all.filter((item) => TIERS.T1.includes(clean(item.brandName)));
+
+        let t1Sereis = HELPERS.cleanForChart(t1Data, "");
+        let t1SereisManu = HELPERS.cleanForChart(t1Data, []);
+
+        let t2Data = result.all.filter((item) => TIERS.T2.includes(clean(item.brandName)));
+        let t2Sereis = HELPERS.cleanForChart(t2Data, "");
+        let t2SereisManu = HELPERS.cleanForChart(t2Data, []);
+
+        let t3Data = result.all.filter((item) => TIERS.T3.includes(clean(item.brandName)));
+        let t3Sereis = HELPERS.cleanForChart(t3Data, "");
+        let t3SereisManu = HELPERS.cleanForChart(t3Data, []);
+
+        console.log('t1Data.length');
+        console.log(t2Data.length);
+        console.log('t1Data.length');
+        console.log(t2Sereis);
+        let data = {
+            //template: { 'shortid': 'rkgavynmhS' }, //,
+            template: { 'shortid': 'BkeIiSXBnS' }, //,
+            data: {
+                sound: result.sound,
+                chart: [
+                    { label: 'Holiday 1 piano sound = DD', y: result.sound.holiday1 },
+                    { label: 'Holiday 2 piano sound = DD+', y: result.sound.holiday2 },
+                    { label: 'Both DD and DD+:', y: result.sound.both },
+                    { label: 'No piano sound:', y: result.sound.no }
+                ],
+
+                t1Sereis: t1Sereis,
+                t1SerPiChart: [
+                    { label: 'With Dolby Tech', y: t1Sereis.DOLBY_AV.yes },
+                    { label: 'No Dolby Tech', y: t1Sereis.DOLBY_AV.no },
+                ],
+                t1SereisManu: t1SereisManu,
+
+                t2Sereis: t2Sereis,
+                t2SerPiChart: [
+                    { label: 'With Dolby Tech', y: t2Sereis.DOLBY_AV.yes },
+                    { label: 'No Dolby Tech', y: t2Sereis.DOLBY_AV.no },
+                ],
+                t2SereisManu: t2SereisManu,
+
+                t3Sereis: t3Sereis,
+                t3SerPiChart: [
+                    { label: 'With Dolby Tech', y: t3Sereis.DOLBY_AV.yes },
+                    { label: 'No Dolby Tech', y: t3Sereis.DOLBY_AV.no },
+                ],
+                t3SereisManu: t3SereisManu,
+
+            },
+        };
+        /*let options = {
+            uri: 'http://localhost:5488/api/report',
+            method: 'POST',
+            json: data
+        };*/
+        let options = {
+            uri: 'https://playground.jsreport.net/w/julius1932/9mFYNpcR/api/report',
+            method: 'POST',
+            json: data
+        };
+        let request = require('request');
+        res.setHeader('Content-Type', 'application/pdf');
+        request(options).pipe(res);
+    });
+
+});
+
+
+app.get(`/books`, function(req, res) {
+    let data = {
+        template: { 'shortid': 'rkgavynmhS' },
+        data: {
+            title: "pppppppppp",
+            chart: [
+                ['Task', 'Hours per Day'],
+                ['Work', 8],
+                ['Friends', 2],
+                ['Eat', 2],
+                ['TV', 2],
+                ['Gym', 2],
+                ['Sleep', 8],
+            ]
+        }
+    };
+    let options = {
+        uri: 'http://localhost:5488/api/report',
+        method: 'POST',
+        json: data
+    }
+
+    let request = require('request');
+    request(options).pipe(res);
+});
+
 if (!module.parent) {
     app.listen(app.get('port'));
     console.log("server listening on port " + app.get('port'));
 }
 
+const clean = (str) => {
+    if (!str) {
+        return "";
+    }
+    //console.log(str);
+    if (typeof str === 'string') {
+        str = str.toUpperCase().trim();
+        str = str.split(".").join("");
+        str = str.split(",").join("");
+        str = str.split("-").join("");
+        str = str.split(" ").join("");
+        str = str.split("/").join("");
+    }
 
+    return str;
+}
 
 module.exports = app;
--- helpers.js
+++ helpers.js
@@ -1,5 +1,16 @@
 const jsonfile = require('jsonfile');
 const gsjson = require('google-spreadsheet-to-json');
+const TIERS = {
+    T1: ['LG', 'SONY', 'SAMSUNG'],
+    T2: ['TCL', 'VU', 'PANASONIC', 'SHARP', 'SKYWORTH', 'PHILIPS', 'HITACHI',
+        'SANSUI', 'JVC'
+    ],
+    T3: ['HAIER', 'LLOYD', 'RECONNECT', 'VU', 'LLOYD', 'ONIDA', 'KORYO', 'IMPEX',
+        'CROMA', 'AMSTRAD', 'MICROMAX', 'ADZEN', 'HENRY', 'IFFALCON', 'INTEX', 'METZ',
+        'MI', 'NVY', 'T-SERIES', 'VISE', 'PANORAMA', 'KODAK - 1', 'AKAI - 3', 'BELTEK - 2',
+        'DETEL - 4', 'MITASHI - 4', 'OSCAR - 1', 'DAIWA'
+    ]
+};
 
 //const LOCAL_HELPERS = {
 const clean = (str) => {
@@ -34,6 +45,7 @@ const readOrderByDist = (data) => {
 }
 const readOrderByBrand = (data, dist) => {
     let items = {};
+    console.log(data.length);
     data.forEach(function(item) {
         let brand = item.brandName;
         brand = clean(brand);
@@ -66,7 +78,8 @@ const readOrderByBrand = (data, dist) => {
     return items;
 }
 const cleanForChart = function(items, dist) {
-    //console.log(items);
+    console.log("items.length");
+    console.log(items.length);
     let series = {
         models: {
             name: "Models",
@@ -85,17 +98,23 @@ const cleanForChart = function(items, dist) {
         console.log("ppppppppppppppppppppppppppp");
         items = jsonfile.readFileSync(`./data.json`);
     }
+    console.log(".........");
+    console.log(items.length);
     if (dist) {
         if (!Array.isArray(dist)) {
             dist = [dist];
         }
-        console.log(dist);
-        dist = dist.map((curr) => curr == "No distributor" ? "" : curr);
-        let cleanedDist = dist.map((curr) => clean(curr));
-        //let cleanedDist = clean(dist);
-        items = items.filter((item) => cleanedDist.includes(clean(item.distributor)));
-
+        if (dist.length > 0) {
+            console.log(dist);
+            dist = dist.map((curr) => curr == "No distributor" ? "" : curr);
+            let cleanedDist = dist.map((curr) => clean(curr));
+            //let cleanedDist = clean(dist);
+            items = items.filter((item) => cleanedDist.includes(clean(item.distributor)));
+        }
     }
+    let totalModels = items.length;
+    console.log("totalModels");
+    console.log(items.length);
     items = readOrderByBrand(items, dist);
 
     let categoriesDist = [];
@@ -105,20 +124,33 @@ const cleanForChart = function(items, dist) {
     if (dist) {
         data = data.sort((a, b) => (a.brand > b.brand) ? 1 : ((b.brand > a.brand) ? -1 : 0));
     }
+    let DOLBY_AV = {
+        yes: 0,
+        no: 0
+    }
+    let brandsWithDolby = [];
+    let brandsWithOutDolby = [];
     data.forEach((item) => {
+        DOLBY_AV.yes += item.dolbyLogo;
         categoriesDist.push(item.brand);
         series.models.data.push(item.models);
         series.pio.data.push(item.pianoPlayed);
         series.tm.data.push(item.dolbyLogo);
-    })
+        if (item.dolbyLogo > 0) {
+            brandsWithDolby.push(item.brand);
+        } else {
+            brandsWithOutDolby.push(item.brand);
+        }
+    });
+    DOLBY_AV.no = totalModels - DOLBY_AV.yes;
     if (dist) {
         categories = categoriesDist;
     }
-    return { categories, series: [series.models, series.pio, series.tm], dist: dist };
+    return { categories, series: [series.models, series.pio, series.tm], dist: dist, DOLBY_AV: DOLBY_AV,brandsWithDolby:brandsWithDolby,brandsWithOutDolby:brandsWithOutDolby };
 }
 
 const HELPERS = {
-    clients: (req, res) => {
+    clientsExcel: (req, res) => {
         let params = req.body;
        // console.log(params);
         let paramsKeys = Object.keys(params);
@@ -154,6 +186,77 @@ const HELPERS = {
                 if (err) {
                     let data = cleanForChart(null, dist);
                     return res.jsonp([]);
+                }
+
+            });
+
+    },
+    clients: (req, res, callback) => {
+        let params = req.query;
+        console.log(params);
+        let paramsKeys = Object.keys(params);
+        paramsKeys = paramsKeys.filter((param) => params[param]);
+        //console.log(paramsKeys);
+        gsjson({
+                spreadsheetId: '1NWNFnVyMZ10AwnwFeAirC5vQNh2MgORywAfRckUFVPw',
+                // other options...
+            })
+            .then(function(result) {
+                console.log(result.length);
+                let data = result.filter((itm) => {
+                    let test = true;
+                    for (let i = 0; i < paramsKeys.length; i++) {
+                        let key = paramsKeys[i];
+                        if (Array.isArray(params[key])) {
+                            let arr = params[key].map((curr) => clean(curr));
+                            test = test && arr.includes(clean(itm[key]));
+                        } else {
+                            //console.log(clean(params[key]) + "========" + clean(itm[key]));
+                            test = test && (clean(params[key]) === clean(itm[key]));
+                            //console.log(test);
+                        }
+                    }
+                    // console.log(test);
+                    return test;
+                });
+                console.log(data.length);
+
+                let sound = {
+                    holiday1: 0,
+                    holiday2: 0,
+                    both: 0,
+                    no: 0
+                }
+                data.forEach((item) => {
+                    let soP = clean(item.pianoSound);
+                    switch (soP) {
+                        case "HOLIDAY1":
+                            sound.holiday1++;
+                            break;
+                        case "HOLIDAY2":
+                            sound.holiday2++;
+                            break;
+                        case "BOTH":
+                            sound.both++;
+                            break;
+                        case "NO":
+                            sound.no++;
+                            break;
+                    }
+
+                })
+                callback({
+                    all: data,
+                    sound: sound
+                });
+
+            })
+            .catch(function(err) {
+                console.log(err.message);
+                console.log(err.stack);
+                if (err) {
+                    //let data = cleanForChart(null, dist);
+                    return {};
                 }
 
             });
@@ -205,6 +308,7 @@ const HELPERS = {
             });
 
     },
+    cleanForChart: cleanForChart
 }
 
 module.exports = HELPERS;
