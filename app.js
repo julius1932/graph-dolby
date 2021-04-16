b8bdd3f1c679c489545f9b69b2e23276a27a713c
DESKTOP-SF1BBLJ\Dynaton Data Zim <lasjulius@gmail.com>
2021-04-16 02:45:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.get(`/client`, function(req, res) {
                     { label: 'Holiday 3 quick check = JPEG', y: result.sound.holiday3 },
                     { label: 'Holiday 4 piano sound = AAC', y: result.sound.holiday4 },
 
-                    { label: 'All DD , DD+ and ACC', y: result.sound.both },
+                    { label: 'All = DD , DD+ and ACC', y: result.sound.both },
                     //{ label: 'No piano sound:', y: result.sound.no }
                 ],
 
